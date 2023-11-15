66f1104653d07f59617541ee3d24fd48bf49a910
vinamyrnauli <vinasllgn@gmail.com>
2023-11-15 16:33:29 +0700

    fix http AGAIN

--- lib/screens/shoplist_form.dart
+++ lib/screens/shoplist_form.dart
@@ -128,7 +128,7 @@ class _ShopFormPageState extends State<ShopFormPage> {
                       // Kirim ke Django dan tunggu respons
                       // OK TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
                       final response = await request.postJson(
-                          "http://rifda-aulia-tutorial.pbp.cs.ui.ac.id/create-flutter/",
+                          "http://vina-myrnauli-tutorial.pbp.cs.ui.ac.id/create-flutter/",
                           jsonEncode(<String, String>{
                             'name': _name,
                             'price': _price.toString(),

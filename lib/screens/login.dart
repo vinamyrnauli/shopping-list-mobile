98b57755ac32ba982e6754c9b1cc3c5e809265f7
vinamyrnauli <vinasllgn@gmail.com>
2023-11-15 16:17:05 +0700

    fix http

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -70,10 +70,11 @@ class _LoginPageState extends State<LoginPage> {
                 // Untuk menyambungkan Android emulator dengan Django pada localhost,
                 // gunakan URL http://10.0.2.2/
                 final response = await request.login(
-                    "http://rifda-aulia-tutorial.pbp.cs.ui.ac.id/auth/login/", {
-                  'username': username,
-                  'password': password,
-                });
+                    "http://vina-myrnauli-tutorial.pbp.cs.ui.ac.id/auth/login/",
+                    {
+                      'username': username,
+                      'password': password,
+                    });
 
                 if (request.loggedIn) {
                   String message = response['message'];
